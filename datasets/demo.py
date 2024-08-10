226e029d2e55a639ef860e185466f4cd089af9a1
WoodwindHu <1484489349@qq.com>
2024-08-11 02:12:16 +0800

    add comments

--- datasets/demo.py
+++ datasets/demo.py
@@ -2,88 +2,77 @@ import numpy as np
 import scipy
 import torch
 from torch.utils.data import Dataset
-from pyDOE import lhs         #Latin Hypercube Sampling
+from pyDOE import lhs  # 导入拉丁超立方采样方法
 
+# 定义一个继承自PyTorch Dataset类的数据集类，用于PINN模型的训练和测试
 class PINN_dataset(Dataset):
-    def __init__(self, filepath, test=False, N_u=100, N_f = 10000):
-        self.test = test
-        data = scipy.io.loadmat(filepath)  	# Load data from file
-        self.x = data['x']                                   # 256 points between -1 and 1 [256x1]
-        self.t = data['t']                                   # 100 time points between 0 and 1 [100x1] 
-        self.usol = data['usol']                             # solution of 256x100 grid points
-        
+    def __init__(self, filepath, test=False, N_u=100, N_f=10000):
+        self.test = test  # 判断是训练集还是测试集
+        data = scipy.io.loadmat(filepath)  # 从.mat文件中加载数据
+        self.x = data['x']  # 从文件中加载x数据，表示空间点
+        self.t = data['t']  # 从文件中加载t数据，表示时间点
+        self.usol = data['usol']  # 从文件中加载解usol，对应于x和t网格点上的解
 
-        self.X, self.T = np.meshgrid(self.x,self.t)                         # makes 2 arrays X and T such that u(X[i],T[j])=usol[i][j] are a tuple
-        ''' X_u_test = [X[i],T[i]] [25600,2] for interpolation'''
-        self.X_u_test = np.hstack((self.X.flatten()[:,None], self.T.flatten()[:,None]))
+        # 创建网格，使得X[i]和T[j]对应u(X[i],T[j])=usol[i][j]
+        self.X, self.T = np.meshgrid(self.x, self.t)  # 创建空间和时间的网格
 
-        # Domain bounds
-        self.lb = self.X_u_test[0]  # [-1. 0.]
-        self.ub = self.X_u_test[-1] # [1.  0.99]
+        # 生成测试数据集X_u_test，将X和T展开为一维数组并水平堆叠
+        self.X_u_test = np.hstack((self.X.flatten()[:, None], self.T.flatten()[:, None]))
 
-        '''
-        Fortran Style ('F') flatten,stacked column wise!
-        u = [c1 
-                c2
-                .
-                .
-                cn]
+        # 定义域的边界
+        self.lb = self.X_u_test[0]  # 下边界
+        self.ub = self.X_u_test[-1]  # 上边界
 
-        u =  [25600x1] 
-        '''
-        self.u_true = self.usol.flatten('F')[:,None] 
-        # 'Generate Training data'
-        self.N_u = N_u #Total number of data points for 'u'
-        self.N_f = N_f #Total number of collocation points 
+        # 将usol展平成列向量，列优先（Fortran风格）
+        self.u_true = self.usol.flatten('F')[:, None]
+
+        # 定义用于训练的数据点数量
+        self.N_u = N_u  # 用于训练u的数据点数量
+        self.N_f = N_f  # 用于训练的配点数量
+
+        # 调用函数生成训练数据，并转换为PyTorch张量
         X_f_train_np_array, X_u_train_np_array, u_train_np_array = self.training_data()
+        self.X_f_train = torch.from_numpy(X_f_train_np_array).float()  # 配点数据
+        self.X_u_train = torch.from_numpy(X_u_train_np_array).float()  # 训练数据中的x和t
+        self.u_train = torch.from_numpy(u_train_np_array).float()  # 训练数据中的u
+        self.X_u_test_tensor = torch.from_numpy(self.X_u_test).float()  # 测试数据中的x和t
+        self.u = torch.from_numpy(self.u_true).float()  # 测试数据中的真实解u
+        self.f_hat = torch.zeros(self.X_f_train.shape[0], 1)  # 初始化f_hat为零向量
 
-        # 'Convert to tensor'
-        self.X_f_train = torch.from_numpy(X_f_train_np_array).float()
-        self.X_u_train = torch.from_numpy(X_u_train_np_array).float()
-        self.u_train = torch.from_numpy(u_train_np_array).float()
-        self.X_u_test_tensor = torch.from_numpy(self.X_u_test).float()
-        self.u = torch.from_numpy(self.u_true).float()
-        self.f_hat = torch.zeros(self.X_f_train.shape[0],1)
-        
-    
     def training_data(self):
-        '''Boundary Conditions'''
+        '''生成用于训练的数据'''
+        # 边界条件数据（初始条件）-1 <= x <= 1 且 t = 0
+        leftedge_x = np.hstack((self.X[0, :][:, None], self.T[0, :][:, None]))  # 左边界数据L1
+        leftedge_u = self.usol[:, 0][:, None]  # 左边界对应的u值
 
-        #Initial Condition -1 =< x =<1 and t = 0  
-        leftedge_x = np.hstack((self.X[0,:][:,None], self.T[0,:][:,None])) #L1
-        leftedge_u = self.usol[:,0][:,None]
+        # 边界条件数据x = -1 且 0 <= t <= 1
+        bottomedge_x = np.hstack((self.X[:, 0][:, None], self.T[:, 0][:, None]))  # 下边界数据L2
+        bottomedge_u = self.usol[-1, :][:, None]  # 下边界对应的u值
 
-        #Boundary Condition x = -1 and 0 =< t =<1
-        bottomedge_x = np.hstack((self.X[:,0][:,None], self.T[:,0][:,None])) #L2
-        bottomedge_u = self.usol[-1,:][:,None]
+        # 边界条件数据x = 1 且 0 <= t <= 1
+        topedge_x = np.hstack((self.X[:, -1][:, None], self.T[:, 0][:, None]))  # 上边界数据L3
+        topedge_u = self.usol[0, :][:, None]  # 上边界对应的u值
 
-        #Boundary Condition x = 1 and 0 =< t =<1
-        topedge_x = np.hstack((self.X[:,-1][:,None], self.T[:,0][:,None])) #L3
-        topedge_u = self.usol[0,:][:,None]
+        # 合并所有边界数据
+        all_X_u_train = np.vstack([leftedge_x, bottomedge_x, topedge_x])  # 合并后的X_u_train [456, 2]
+        all_u_train = np.vstack([leftedge_u, bottomedge_u, topedge_u])  # 对应的u值 [456, 1]
 
-        all_X_u_train = np.vstack([leftedge_x, bottomedge_x, topedge_x]) # X_u_train [456,2] (456 = 256(L1)+100(L2)+100(L3))
-        all_u_train = np.vstack([leftedge_u, bottomedge_u, topedge_u])   #corresponding u [456x1]
+        # 随机选取N_u个点作为训练数据
+        idx = np.random.choice(all_X_u_train.shape[0], self.N_u, replace=False)  # 随机选取的索引
+        X_u_train = all_X_u_train[idx, :]  # 选取的训练点(x, t)
+        u_train = all_u_train[idx, :]  # 对应的u值
 
-        #choose random N_u points for training
-        idx = np.random.choice(all_X_u_train.shape[0], self.N_u, replace=False) 
+        '''配点采样'''
+        # 使用拉丁超立方采样法生成N_f个配点
+        X_f_train = self.lb + (self.ub - self.lb) * lhs(2, self.N_f)
+        X_f_train = np.vstack((X_f_train, X_u_train))  # 将训练点添加到配点中
 
-        X_u_train = all_X_u_train[idx, :] #choose indices from  set 'idx' (x,t)
-        u_train = all_u_train[idx,:]      #choose corresponding u
-
-        '''Collocation Points'''
-
-        # Latin Hypercube sampling for collocation points 
-        # N_f sets of tuples(x,t)
-        X_f_train = self.lb + (self.ub-self.lb)*lhs(2,self.N_f) 
-        X_f_train = np.vstack((X_f_train, X_u_train)) # append training points to collocation points 
-
-        return X_f_train, X_u_train, u_train 
-
+        return X_f_train, X_u_train, u_train  # 返回配点，训练点及其对应的u值
 
     def __getitem__(self, index):
         if self.test:
-            return self.X_u_test_tensor, self.u
-        return self.X_u_train, self.u_train, self.X_f_train, self.f_hat
+            return self.X_u_test_tensor, self.u  # 如果是测试集，返回测试数据
+        return self.X_u_train, self.u_train, self.X_f_train, self.f_hat  # 返回训练数据
 
     def __len__(self):
-        return 250
+        return 250  # 返回数据集的大小，这里设定为250
--- models/sequence.py
+++ models/sequence.py
@@ -2,137 +2,103 @@ import numpy as np
 import torch
 from torch import nn
 import pytorch_lightning as pl
-import torch.autograd as autograd         # computation graph
+import torch.autograd as autograd  # 导入自动求导工具，用于计算梯度
 
-
+# 定义一个继承自pytorch_lightning的LightningModule的类Sequentialmodel
 class Sequentialmodel(pl.LightningModule):
     def __init__(self, layers, ub, lb):
-        super().__init__()  # call __init__ from parent class
-        self.layers = layers
-        self.u_b = torch.from_numpy(ub).float()
-        self.l_b = torch.from_numpy(lb).float()
-        # 'activation function'
-        self.activation = nn.Tanh()
-        # 'loss function'
-        self.loss_function = nn.MSELoss(reduction='mean')
-        # 'Initialise neural network as a list using nn.Modulelist'
+        super().__init__()  # 调用父类的__init__方法，进行初始化
+        self.layers = layers  # 保存神经网络的层结构
+        self.u_b = torch.from_numpy(ub).float()  # 将上界ub从numpy数组转为torch张量，并转换为浮点型
+        self.l_b = torch.from_numpy(lb).float()  # 将下界lb从numpy数组转为torch张量，并转换为浮点型
+        self.activation = nn.Tanh()  # 定义激活函数为Tanh
+        self.loss_function = nn.MSELoss(reduction='mean')  # 定义损失函数为均方误差损失函数
+        # 使用nn.ModuleList初始化神经网络层，创建一个包含线性层的列表
         self.linears = nn.ModuleList(
             [nn.Linear(layers[i], layers[i+1]) for i in range(len(layers)-1)])
 
-        # 'Xavier Normal Initialization'
+        # 使用Xavier Normal初始化方法初始化权重
         for i in range(len(layers)-1):
-            # weights from a normal distribution with
-            # Recommended gain value for tanh = 5/3?
+            # 使用Xavier Normal初始化权重，并设置增益系数为1.0
             nn.init.xavier_normal_(self.linears[i].weight.data, gain=1.0)
-            # set biases to zero
-            nn.init.zeros_(self.linears[i].bias.data)
+            nn.init.zeros_(self.linears[i].bias.data)  # 将偏置项初始化为零
 
-    #'foward pass'
+    # 定义前向传播方法
     def forward(self, x):
-        if torch.is_tensor(x) != True:
-            x = torch.from_numpy(x)
+        if not torch.is_tensor(x):  # 如果输入不是torch张量
+            x = torch.from_numpy(x)  # 将输入从numpy数组转换为torch张量
 
+        # 对输入进行预处理，进行特征缩放
+        x = (x - self.l_b.to(x.device)) / (self.u_b.to(x.device) - self.l_b.to(x.device))
 
-        # preprocessing input
-        x = (x - self.l_b.to(x.device))/(self.u_b.to(x.device) - self.l_b.to(x.device))  # feature scaling
+        a = x.float()  # 将输入转换为浮点型
 
-        # convert to float
-        a = x.float()
+        # 逐层进行前向传播
+        for i in range(len(self.layers) - 2):
+            z = self.linears[i](a)  # 进行线性变换
+            a = self.activation(z)  # 通过激活函数
 
-        '''     
-        Alternatively:
-        
-        a = self.activation(self.fc1(a))
-        a = self.activation(self.fc2(a))
-        a = self.activation(self.fc3(a))
-        a = self.fc4(a)
-        
-        '''
+        a = self.linears[-1](a)  # 最后一层不经过激活函数，直接输出
 
-        for i in range(len(self.layers)-2):
-            z = self.linears[i](a)
-            a = self.activation(z)
+        return a  # 返回最终输出
 
-        a = self.linears[-1](a)
-
-        return a
-
+    # 定义边界条件损失函数
     def loss_BC(self, x, y):
-        loss_u = self.loss_function(self.forward(x), y)
+        loss_u = self.loss_function(self.forward(x), y)  # 计算模型预测值与真实值之间的损失
         return loss_u
 
+    # 定义偏微分方程(PDE)损失函数
     def loss_PDE(self, x_to_train_f, f_hat):
-        nu = 0.01/np.pi
-        x_1_f = x_to_train_f[:, [0]]
-        x_2_f = x_to_train_f[:, [1]]
-        g = x_to_train_f.clone()
-        g.requires_grad = True
-        u = self.forward(g)
+        nu = 0.01 / np.pi  # 定义粘性系数
+        x_1_f = x_to_train_f[:, [0]]  # 提取第一个输入变量
+        x_2_f = x_to_train_f[:, [1]]  # 提取第二个输入变量
+        g = x_to_train_f.clone()  # 克隆输入数据
+        g.requires_grad = True  # 设置需要计算梯度
+        u = self.forward(g)  # 通过模型进行前向传播，得到预测值
         u_x_t = autograd.grad(u, g, torch.ones([*x_to_train_f.shape[0:-1], 1]).to(
-            x_to_train_f.device), retain_graph=True, create_graph=True)[0]
+            x_to_train_f.device), retain_graph=True, create_graph=True)[0]  # 计算u对g的梯度
 
         u_xx_tt = autograd.grad(u_x_t, g, torch.ones(
-            x_to_train_f.shape).to(x_to_train_f.device), create_graph=True)[0]
+            x_to_train_f.shape).to(x_to_train_f.device), create_graph=True)[0]  # 计算二阶导数
 
-        u_x = u_x_t[..., [0]]
-        u_t = u_x_t[..., [1]]
-        u_xx = u_xx_tt[..., [0]]
-        f = u_t + (self.forward(g))*(u_x) - (nu)*u_xx
-        loss_f = self.loss_function(f, f_hat)
+        u_x = u_x_t[..., [0]]  # 提取对x的导数
+        u_t = u_x_t[..., [1]]  # 提取对t的导数
+        u_xx = u_xx_tt[..., [0]]  # 提取对x的二阶导数
+        f = u_t + (self.forward(g)) * u_x - (nu) * u_xx  # 定义PDE方程
+        loss_f = self.loss_function(f, f_hat)  # 计算PDE方程的损失
 
         return loss_f
 
+    # 定义总损失函数，结合边界条件损失和PDE损失
     def loss(self, x, y, x_to_train_f, f_hat):
+        loss_u = self.loss_BC(x, y)  # 计算边界条件损失
+        loss_f = self.loss_PDE(x_to_train_f, f_hat)  # 计算PDE损失
 
-        loss_u = self.loss_BC(x, y)
-        loss_f = self.loss_PDE(x_to_train_f, f_hat)
-
-        self.log("loss_u", loss_u)
-        self.log("loss_f", loss_f)
-        loss_val = loss_u + loss_f
+        self.log("loss_u", loss_u)  # 记录边界条件损失
+        self.log("loss_f", loss_f)  # 记录PDE损失
+        loss_val = loss_u + loss_f  # 总损失为两者之和
 
         return loss_val
 
+    # 定义训练步骤
     def training_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        # it is independent of forward
-        X_u_train, u_train, X_f_train, f_hat = batch
-        loss = self.loss(X_u_train, u_train, X_f_train, f_hat)
-        return loss
-    
+        X_u_train, u_train, X_f_train, f_hat = batch  # 解包batch数据
+        loss = self.loss(X_u_train, u_train, X_f_train, f_hat)  # 计算总损失
+        return loss  # 返回损失
+
+    # 定义测试步骤
     def test_step(self, batch, batch_idx):
-        # this is the test loop
-        X_u_test_tensor, u = batch
-        u_pred = self.forward(X_u_test_tensor)
-        test_loss = (torch.norm((u-u_pred),2)/torch.norm(u,2)).mean()
-        self.log("test_loss", test_loss)
-    
+        X_u_test_tensor, u = batch  # 解包测试数据
+        u_pred = self.forward(X_u_test_tensor)  # 进行前向传播，得到预测值
+        test_loss = (torch.norm((u - u_pred), 2) / torch.norm(u, 2)).mean()  # 计算测试损失
+        self.log("test_loss", test_loss)  # 记录测试损失
+
+    # 定义优化器配置
     def configure_optimizers(self):
         optimizer = torch.optim.LBFGS(self.parameters(), lr=0.1, 
-                              max_iter = 250, 
-                              max_eval = None, 
-                              tolerance_grad = 1e-05, 
-                              tolerance_change = 1e-09, 
-                              history_size = 100, 
-                              line_search_fn = 'strong_wolfe')
-        return optimizer
-    
-    # 'callable for optimizer'
-    # def closure(self):
-    #     optimizer.zero_grad()
-    #     loss = self.loss(X_u_train, u_train, X_f_train)
-    #     loss.backward()
-    #     self.iter += 1
-    #     if self.iter % 100 == 0:
-    #         error_vec, _ = PINN.test()
-    #         print(loss, error_vec)
-    #     return loss
-
-    # 'test neural network'
-    # def test(self):
-    #     u_pred = self.forward(X_u_test_tensor)
-    #     # Relative L2 Norm of the error (Vector)
-    #     error_vec = torch.linalg.norm((u-u_pred), 2)/torch.linalg.norm(u, 2)
-    #     u_pred = u_pred.cpu().detach().numpy()
-    #     u_pred = np.reshape(u_pred, (256, 100), order='F')
-    #     return error_vec, u_pred
+                              max_iter=250, 
+                              tolerance_grad=1e-05, 
+                              tolerance_change=1e-09, 
+                              history_size=100, 
+                              line_search_fn='strong_wolfe')  # 使用LBFGS优化器
+        return optimizer  # 返回优化器
